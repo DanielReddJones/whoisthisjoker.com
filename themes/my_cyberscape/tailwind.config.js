ccc15809541d60d9496990d579177c8b7e44e763
Daniel Jones <danielreddjones@protonmail.com>
2024-10-24 14:19:19 -0500

    third time is the charm

---
+++ themes/my_cyberscape/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md", "../../jokerlinks/**/*.md", "../../content/**"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
